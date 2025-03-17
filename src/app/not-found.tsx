8181f019a97bbf6e4142a449755ca5fbcc4015ef
qslxs <qslxs@126.com>
2025-03-17 15:00:36 +0800

    init two

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
         <div className="max-w-md">
           <div className="text-5xl font-dark font-bold">404</div>
           <p className="text-2xl md:text-3xl font-light leading-normal">
-            Sorry we couldn't find this page.{" "}
+            Sorry we couldn&apos;t find this page.{" "}
           </p>
           
           <Link href="/" className="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700">
